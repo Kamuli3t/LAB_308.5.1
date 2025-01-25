fb3a07484ccd19cc6d2a0fa3b9079fbe4fbe7792
kamuli3t <kamuli3t@gmail.com>
2025-01-24 17:25:04 -0800

    Done - Part 1 - Take an array of numbers and return the average

---
+++ index.js
@@ -0,0 +1,25 @@
+//! Part-1
+//? which would make the most sense to you?
+//* I would pick this source code for it's readablity,
+//*  and used common string methods to do the job!
+function reverseString2(str) {
+  return str.split("").reverse().join("");
+}
+
+//* Take an array of numbers and return the sum
+
+function arrSum(arr) {
+  arr.reduce((acc, el) => {
+    acc += el;
+    return acc;
+  }, 0);
+}
+
+//* Take an array of numbers and return the average
+
+function arrAve(arr) {
+  arr.reduce((acc, el) => {
+    acc += el;
+    return acc / arr.legnth;
+  }, 0);
+}
